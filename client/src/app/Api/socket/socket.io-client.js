f84f1c54b60b2a3a657eacb130b936be41a5727b
aryanrajput9 <babusahebaryan4@gmail.com>
2026-09-02 09:10:51 +0530

    update

--- client/src/app/Api/socket/socket.io-client.js
+++ client/src/app/Api/socket/socket.io-client.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-const socketClient = io("http://localhost:3000")
+const socketClient = io("https://erp-system-4-00v2.onrender.com")
 
 
 export default socketClient
